cd466b142acbcc61898302c0b6aad5244aff7052
Syr1tsa <33559034+Syr1tsa@users.noreply.github.com>
2020-08-20 21:26:53 +0300

    changed paths

--- js/main.js
+++ js/main.js
@@ -8,14 +8,14 @@ const web__btn = document.querySelector('#web__btn'),
       work.className = "col-lg-6 text-center";
       work.innerHTML = `
                       <div id="web" id="work" class="portfolio-block__work">
-                  <img src="/img/portfolio/1.png" width="100%" alt="work_1">
+                  <img src="./img/portfolio/1.png" width="100%" alt="work_1">
                   <p>Isometric Perspective Mock-Up</p>
                 </div>`;
       work_2 = document.createElement('div');
       work_2.className = "col-lg-6 text-center";
       work_2.innerHTML = `
       <div id="apps" class="portfolio-block__work">
-      <img src="/img/portfolio/2.png" width="100%" alt="work_2">
+      <img src="./img/portfolio/2.png" width="100%" alt="work_2">
       <p>Time Zone App UI</p>
     </div>`;
 
